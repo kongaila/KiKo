7475d42a151936546fbb02df54e36edbd00e20c4
孔孔孔 <1941765664@qq.com>
2020-12-04 18:15:02 +0800

    redis部分操作

--- server/redis.go
+++ server/redis.go
@@ -1,1 +1,13 @@
 package server
+
+import "github.com/garyburd/redigo/redis"
+
+func GetString(key string) (string, error) {
+	r := Cache.Get()
+	return redis.String(r.Do("get", key))
+}
+
+func SetString(key, value string) (string, error) {
+	r := Cache.Get()
+	return redis.String(r.Do("set", key, value))
+}
